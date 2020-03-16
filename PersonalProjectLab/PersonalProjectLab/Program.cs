8b59f5637dc4cb20fee3eded9a0de917927e667b
SamuelMHarvey <59981663+SamuelMHarvey@users.noreply.github.com>
2020-03-15 23:32:46 -0500

    Added classes

--- PersonalProjectLab/PersonalProjectLab/Program.cs
+++ PersonalProjectLab/PersonalProjectLab/Program.cs
@@ -6,7 +6,16 @@ namespace PersonalProjectLab
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Dungeons and Dragons based combat gives each combatant an armor class that represents how hard they are to hit. When attacking, the player has to roll a die to see if they can hit it
+            // Player class with health and armor class
+            // Enemy class with health and armor class randomly decided
+            // Player class includes methods for determining attack roll and damage
+            // Enemy class includes methods for armor class and health
+            // If attack roll is higher than armor class attack hits
+            // Damage reduces health
+            // If enemy health is below 0, enemy dies
+            // When enemy dies the player gets rewards
+            // If the player dies they restart 
         }
     }
 }
