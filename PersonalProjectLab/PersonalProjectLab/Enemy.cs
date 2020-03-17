3a65491a6d0ab2b052742226536c23b56f214468
SamuelMHarvey <59981663+SamuelMHarvey@users.noreply.github.com>
2020-03-16 22:39:42 -0500

    Added variables

--- PersonalProjectLab/PersonalProjectLab/Enemy.cs
+++ PersonalProjectLab/PersonalProjectLab/Enemy.cs
@@ -6,5 +6,17 @@ namespace PersonalProjectLab
 {
     class Enemy
     {
+        int _enemyHealth;
+        int _armorClass;
+
+        public Enemy(int enemyHealth, int armorClass)
+        {
+            int _enemyHealth = enemyHealth;
+        }
+
+        public int EnemyHealth()
+        {
+            return _enemyHealth;
+        }
     }
 }
--- PersonalProjectLab/PersonalProjectLab/Player.cs
+++ PersonalProjectLab/PersonalProjectLab/Player.cs
@@ -6,5 +6,31 @@ namespace PersonalProjectLab
 {
     class Player
     {
+        int _playerHealth;
+        int _armorClass;
+        int _playerWeaponDamage;
+
+        public Player(int playerHealth, int armorClass, int playerWeaponDamage)
+        {
+            int _playerHealth = playerHealth;
+            int _armorClass = armorClass;
+            int _playerWeaponDamage = playerWeaponDamage;
+        }
+
+        public int PlayerHealth()
+        {
+            return _playerHealth;
+        }
+
+        public int PlayerDamage()
+        {
+            return RandomNumber(1, 4) + _playerWeaponDamage;
+        }
+
+        private int RandomNumber(int min, int max)
+        {
+            Random random = new Random();
+            return random.Next(min, max);
+        }
     }
 }
